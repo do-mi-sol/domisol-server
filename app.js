1c36282ea4a12f069f8fa808d3cd86fd569911cb
HeeYeonKim98 <wow9896@naver.com>
2020-09-23 14:39:46 +0900

    update config

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+//db 생성 및 연결
+const mysql = require("mysql");
+const db_config = require("./config/database/db_config");
+const connection = mysql.createConnection(db_config);
+
 const app = express();
 
 app.use(express.json());
@@ -10,6 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json("시작");
+});
+
+app.get("/info", (req, res) => {
+  connection.query("SELECT * from testdb", (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
+  });
 });
 
 app.listen("9001", () => {
---
+++ config/database/db_config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  host: "localhost",
+  user: "root",
+  password: "gmldus98!",
+  database: "testDB",
+};
