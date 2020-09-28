abcf2190611d0e170a1bad7f3a130f7596b4fc13
HeeYeonKim98 <wow9896@naver.com>
2020-09-29 03:41:55 +0900

    update logincontrol

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,31 +1,43 @@
-const express = require("express")
-const { createConnection } = require("mysql")
+const express = require("express");
+const { createConnection } = require("mysql");
 
 module.exports = {
-    login: (res, req, next) => {},
-    signup: (res, req, next) => {
-        var users = {
-            user_id: req.body.user_id,
-            name: req.body.name,
-            password: req.body.password,
-            email: req.body.email,
-            age: req.body.age,
-        }
-        createConnection.query("INSERT INTO users SET ?", users, function (error, results, fields) {
-            if (error) {
-                console.log(error)
-                res.send({
-                    code: 400,
-                    result: "error",
-                })
-            } else {
-                console.log("solution :", results)
-                res.send({
-                    code: 400,
-                    result: "success",
-                })
-            }
-        })
-        next()
-    },
-}
+  login: (res, req, next) => {
+    const { id, name, password, email, age } = req.body;
+    if (id == null || password == null)
+      return onmessageerror("error", res, req);
+
+    try {
+    } catch {}
+  },
+
+  signup: (res, req, next) => {
+    const users = {
+      user_id: req.body.user_id,
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      age: req.body.age,
+    };
+    createConnection.query("INSERT INTO users SET ?", users, function (
+      error,
+      results,
+      fields
+    ) {
+      if (error) {
+        console.log(error);
+        res.send({
+          code: 400,
+          result: "error",
+        });
+      } else {
+        console.log("solution :", results);
+        res.send({
+          code: 400,
+          result: "success",
+        });
+      }
+    });
+    next();
+  },
+};
