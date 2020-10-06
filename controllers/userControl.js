9f4a326928926bc4e53109c06228dc3da2f3973d
HanJeong Seol <hanjeong94@naver.com>
2020-10-06 17:39:37 +0900

    가입 중복체크, 패스워드 암호화

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -3,71 +3,101 @@ const express = require("express")
 //DB connect
 // const db = require("../config/database/db_config");
 const conn = require("../config/database/db_connect")().init()
+const bcrypt = require("bcrypt")
+const e = require("express")
+const jwt = require("jsonwebtoken")
+const YOUR_SECRET_KEY = process.env.SECRET_KEY
+
+const saltRounds = 10
 
 module.exports = {
     login: (req, res, next) => {
         let user_id = req.body.user_id
         let password = req.body.password
         let sql = `SELECT * FROM users WHERE user_id =?`
-        conn.query(sql, [user_id], function (error, result) {
-            console.log("result :", result)
+        conn.query(sql, [user_id], function (error, results) {
             if (error) {
-                res.json({
+                return res.json({
                     code: 400,
                     success: false,
                     error,
                 })
-            } else {
-                if (!result[0]) {
-                    res.status(202).json({
-                        success: false,
-                        message: "아이디를 확인해주세요.",
-                    })
-                } else {
-                    if (result[0].password == password) {
-                        console.log("아이디와 패스워드가 일치합니다.")
-                        res.status(200).json({
-                            success: true,
-                            message: "아이디와 패스워드가 일치합니다.",
-                        })
-                    } else {
-                        console.log("패스워드가 일치하지 않습니다.")
-                        res.status(202).json({
+            }
+            if (!results[0]) {
+                return res.status(202).json({
+                    success: false,
+                    message: "아이디를 확인해주세요.",
+                })
+            }
+            const checkPw = () => {
+                // bcrypt.compare 콜백함수에서 res는 true or false를 반환해준다.
+                // 암호화된 패스워드 체크
+                bcrypt.compare(password, results[0].password, function (err, isMatch) {
+                    if (err)
+                        return res.status(400).json({
                             success: false,
-                            message: "패스워드가 일치하지 않습니다.",
+                            err,
+                        })
+                    if (isMatch) {
+                        console.log("아이디 패스워드가 일치합니다.")
+                        return res.status(200).json({
+                            seccess: true,
+                            message: "아이다와 패스워드가 일치합니다.",
                         })
                     }
-                }
+                })
             }
+            checkPw()
         })
-        next()
     },
 
     signup: (req, res, next) => {
-        // 데이터 들어왔는지 확인
-        // console.log(req.body)
         var users = {
             user_id: req.body.user_id,
             name: req.body.name,
             password: req.body.password,
-            email: req.body.emai,
+            email: req.body.email,
             age: req.body.age,
         }
 
-        // const { id, name, password, email, age } = req.body;
-
-        var sql = `INSERT INTO users SET ?`
-        conn.query(sql, users, function (error, result) {
+        // const { id, name, password, email, age } = req.body
+        var search_sql = `SELECT user_id FROM users WHERE user_id =?`
+        // conn.query_0 start
+        conn.query(search_sql, users.user_id, async (error, results) => {
             if (error) {
-                return res.json({
-                    code: 200,
-                    success: false,
-                    error,
-                })
+                return res.status(400).json({ success: false, error })
             } else {
-                return res.status(200).json({ success: true })
+                if (results[0]) {
+                    if (result[0].user_id === users.user_id) {
+                        console.log("이미 동일한 아이디가 존재합니다.")
+                        return res.status(202).json({
+                            seccess: false,
+                            message: "이미 동일한 아이디가 존재합니다.",
+                        })
+                    }
+                } // 이미 가입된 회원 있는지 확인
+                bcrypt.genSalt(saltRounds, (err, salt, next) => {
+                    if (err) return next(err)
+                    bcrypt.hash(users.password, salt, (err, hash) => {
+                        if (err) return next(err)
+                        users.password = hash
+                        var sql = `INSERT INTO users SET ?`
+                        // conn.query_1 start
+                        conn.query(sql, users, function (error, results) {
+                            if (error) {
+                                return res.json({
+                                    code: 400,
+                                    success: false,
+                                    error,
+                                })
+                            } else {
+                                return res.status(200).json({ success: true })
+                            }
+                        }) // conn.query_1, 중복 회원 없을 시 db저장
+                    })
+                })
             }
-        })
+        }) // conn.query_0
         next()
     },
 }
