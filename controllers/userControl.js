423aff69738f322a19534340a831ac0cb2bca55e
HeeYeonKim98 <wow9896@naver.com>
2020-10-13 20:53:39 +0900

    complete : login,signup

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,15 +1,12 @@
-const express = require("express");
 const bcrypt = require("bcrypt");
-// const {domisolSalt} = require("../config/etc/etc_config")
+
 const pool = require("../config/database/db_connect");
 const SQL = require("../config/database/db_sql");
 
 const { errorMsg } = require("../utils/myMessage");
 
 const compare = async (userPassword, dbPassword) => {
-    const c = bcrypt.compareSync(userPassword, dbPassword);
-    console.log("야야야야야야ㅑ야야야야" + c);
-    return c;
+    return bcrypt.compareSync(userPassword, dbPassword);
 };
 
 module.exports = {
@@ -56,44 +53,3 @@ module.exports = {
         next();
     },
 };
-
-// if (user_id == null && password == null) {
-//   return myMessage(500, false, "실패패");
-// }
-
-// let sql = `SELECT * FROM users WHERE user_id =?`;
-// conn.query(sql, [user_id], function (error, results) {
-//   if (error) {
-//     return res.json({
-//       code: 400,
-//       success: false,
-//       error,
-//     });
-//   }
-//   if (!results[0]) {
-//     return res.status(202).json({
-//       success: false,
-//       message: "아이디를 확인해주세요.",
-//     });
-//   }
-//   const checkPw = () => {
-//     // bcrypt.compare 콜백함수에서 isMatch는 true or false를 반환해준다.
-//     // 암호화된 패스워드 체크
-//     // test_1
-//     bcrypt.compare(password, results[0].password, function (err, isMatch) {
-//       if (err)
-//         return res.status(400).json({
-//           success: false,
-//           err,
-//         });
-//       if (isMatch) {
-//         console.log("아이디 패스워드가 일치합니다.");
-//         return res.status(200).json({
-//           seccess: true,
-//           message: "아이다와 패스워드가 일치합니다.",
-//         });
-//       }
-//     });
-//   };
-//   checkPw();
-// });
