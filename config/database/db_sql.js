11125da0908c687243e2893cea1784b046ad373e
HeeYeonKim98 <wow9896@naver.com>
2020-10-21 17:07:20 +0900

    heroku sql 수정

--- config/database/db_sql.js
+++ config/database/db_sql.js
@@ -1,7 +1,7 @@
 //user
 const INSERT_all = "INSERT INTO user SET ?";
 
-const SELECT_userid = "SELECT * FROM user WHERE user_id='?' ;";
+const SELECT_userid = "SELECT * FROM user WHERE user_id=? ;";
 const SELECT_email = "SELECT * FROM user WHERE email=? ;";
 const SELECT_password = "SELECT * FROM user WHERE password=? ;";
 const SELECT_userid_password = "SELECT * FROM user WHERE user_id=? and password=?;";
